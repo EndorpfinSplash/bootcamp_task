3ed0b5f760959c9bf64dfa8c463334e3d698dc5b
Andrey Zinovich <Andrey.Zinovich@life.com.by>
2023-03-27 04:56:19 +0300

    Exception management

---
+++ src/main/java/com/bootcamp/userendpoint/controllers/advice/ExceptionControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.bootcamp.userendpoint.controllers.advice;
+
+import com.bootcamp.userendpoint.exceptions.*;
+import com.bootcamp.userendpoint.model.ErrorDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+    @ExceptionHandler(InvalidEmailAddressException.class)
+    public ResponseEntity<ErrorDetails> exceptionInvalidEmailAddressHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Получен не корректный адрес электронной почты!");
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<ErrorDetails> exceptionRoleNotFoundHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Полученная роль отсутствует!");
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+}
